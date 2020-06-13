674ed3662ad4421c5f2257df7decbbab50758797
June YAMAMOTO <june@yambal.net>
2020-06-14 00:40:34 +0900

    Updates

--- precache-manifest.3de76dbc390cdbca741bb3c859cc4837.js
+++ precache-manifest.3de76dbc390cdbca741bb3c859cc4837.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1bd31cebabfaddf038da71840e75b93c",
+    "revision": "74f88fb788f3c5a11755ded15957d591",
     "url": "./index.html"
   },
   {
-    "revision": "151c10f342d5c588b820",
-    "url": "./static/js/2.688aed76.chunk.js"
+    "revision": "26e37e34ea52511856f6",
+    "url": "./static/js/2.59f5690b.chunk.js"
   },
   {
     "revision": "93a95f963f54f3d6f73c849dcbc011d9",
-    "url": "./static/js/2.688aed76.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.59f5690b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58ef5cd46b3f9a65b9c0",
-    "url": "./static/js/main.c9b173de.chunk.js"
+    "revision": "f29e7c584ce333e719db",
+    "url": "./static/js/main.4e8ecbc4.chunk.js"
   },
   {
     "revision": "33af829b2126bbebb255",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8e3a4d8a17b8fcc85039653c92ac02e7.js"
+  "./precache-manifest.3de76dbc390cdbca741bb3c859cc4837.js"
 );
 
 self.addEventListener('message', (event) => {
